6fb597850ebe8c26120cddaa5a31e393d0bd23ec
wilfredo <villcaf175@gmail.com>
2024-06-30 22:31:59 -0400

    Agregado la ubicacion de la cancha

--- SistemaWeb/Program.cs
+++ SistemaWeb/Program.cs
@@ -37,6 +37,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Login}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
