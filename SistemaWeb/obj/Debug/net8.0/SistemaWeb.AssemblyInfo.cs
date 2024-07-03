4fb1417bd9b15d5116766ba934bb11d1adbe3d95
David <roquedavid000@gmail.com>
2024-07-03 14:23:51 -0400

    modificacion cancha

--- SistemaWeb/obj/Debug/net8.0/SistemaWeb.AssemblyInfo.cs
+++ SistemaWeb/obj/Debug/net8.0/SistemaWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SistemaWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a4f5f358c81ac317e3669340b39125211a4cb9d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+862eff5835c3e328da58e9ac6bbfbaf5dde10342")]
 [assembly: System.Reflection.AssemblyProductAttribute("SistemaWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SistemaWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
