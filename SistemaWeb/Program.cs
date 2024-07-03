76b5342fec575a3ee71781e1a6fcef543f78843c
Ronald-MC <ronaldmamani840@gmail.com>
2024-07-03 17:12:11 -0400

    modificacion de balsamic

--- SistemaWeb/Program.cs
+++ SistemaWeb/Program.cs
@@ -37,6 +37,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=login}/{action=Index}/{id?}");
 
 app.Run();
